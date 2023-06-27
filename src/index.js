18971d2973d1806a81e058170b1c3680b5f047b6
Mikhailo Salko <slipknotmisha@gmail.com>
2023-06-27 21:14:28 +0300

    add import browser routrer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from 'components/App';
 import './index.css';
 import { store } from 'redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
